c6ec06c4862ff8ec4b0aae33bcf2313f85869750
Andre Capolupo <andre.capolupo@gmail.com>
2024-10-01 10:40:13 -0300

    Commit - Criando o ícone da categoria

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ const amount = document.getElementById("amount") //Elemento tem id
 const expense = document.getElementById("expense") //Elemento tem id despesa
 const category = document.getElementById("category") //Elemento tem id despesa
 
+//Seleciona os elementos da lista
+const expenseList = document.querySelector("ul")
+
+
 amount.oninput = () => {
   //Estou recebendo o valor do input tirando as letras e...
   let value = amount.value.replace(/\D/g, "")
@@ -26,7 +30,6 @@ function formatCurrencyBRL(value){
   return value
 }
 
-
 //Criar o evento de formulario
 //Captura o evento de submit do formulario para obter os valores
 form.onsubmit = (event) => {
@@ -37,7 +40,7 @@ form.onsubmit = (event) => {
   const newExpense = {
     id: new Date().getTime(),
     expense: expense.value, // Estou pegando do próprio input o valor
-    category: category.value,
+    category_id: category.value,
     category_name: category.options[category.selectedIndex].text,//pegando apenas a opção selecionada
     amount: amount.value,
     created_at: new Date(),
@@ -52,7 +55,17 @@ function expenseAdd(newExpense){
   try {
     //Cria o elemento li para adicionar o item na lista ul
     const expenseItem = document.createElement("li")
-    expenseItem.classList.add(expense)
+    expenseItem.classList.add("expense")
+
+    //Cria o icone da categoria
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    //Adiciona as informações no item
+    expenseItem.append(expenseIcon)
+    expenseList.append(expenseItem)
+
   } catch (error) {
     alert("Não foi possivel atualizar a lista de despesas")
     console.log(error)
